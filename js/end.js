49a4081b2ae5d0ac4613ecb34cb8ca26829a1aa2
Rebeldan24 <djsol86@gmail.com>
2023-01-25 19:58:09 +0000

    add

--- js/end.js
+++ js/end.js
@@ -5,7 +5,7 @@ const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
-const MAX_HIGH_SCORES = 3
+const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
 
@@ -28,7 +28,7 @@ highScores.sort((a,b) => {
     return b.score - a.score
 })
 
-highScores,splice(5)
+highScores.splice(5)
 
 localStorage.setItem('highScores', JSON.stringify(highScores))
-window.location.assign('/')
+window.location.assign('/end.html')
